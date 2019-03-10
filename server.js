79530761aa8ee0576b0c60e9909252ad83c73212
Jake Holtz <jahholtz@gmail.com>
2019-03-09 19:13:09 -0800

    Set up basic React boilerplate

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => { console.log(`App is listening on port ${port}`) });
+
+app.get('/', (req, res) => {
+  console.log('Connected');
+});
+ 
